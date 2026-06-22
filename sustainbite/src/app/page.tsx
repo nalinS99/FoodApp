d823301ca4d77c0f07b8bc2757ef95411631e2c0
nalin <rmnsbandara@std.appsc.sab.ac.lk>
2026-06-22 14:42:52 +0530

    Variable setup

--- sustainbite/src/app/page.tsx
+++ sustainbite/src/app/page.tsx
@@ -4,8 +4,8 @@ export default function Home() {
   return (
     
       <main className="flex flex-col  w-full h-full justify-center my-auto ">
-        <h1 className="text-[#006D37] text-6xl font-extrabold  mx-auto">Sustain Bite</h1>
-        <h2 className="text-[#000b06] text-2xl  mx-auto">Comming Soon....</h2>
+        <h1 className="text-primary text-title-3x-large  font-extrabold  mx-auto">Sustain Bite</h1>
+        <h2 className="text-[#000b06] text-body-xs  mx-auto">Comming Soon....</h2>
         
         
       </main>
